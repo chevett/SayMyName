065172d16a465e5b61614f6999d214745f9107b9
Mike Chevett <mchevett@gmail.com>
2013-02-25 08:34:47 -0500

    jhghgjh

---
+++ SayMyName/SayMyName.Models/SlaveBrowser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmallDataStore;
+
+namespace SayMyName.Models
+{
+	public class SlaveBrowser : ISmallStorageItem
+	{
+		public string Fingerprint { get; set; }
+		public List<string> IpAddress { get; set; }
+
+
+		string ISmallStorageItem.GetKey()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ SayMyName/SayMyName.Models/SlavePage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmallDataStore;
+
+namespace SayMyName.Models
+{
+	public class SlavePage : ISmallStorageItem
+	{
+		public string Url { get; set; }
+
+		string ISmallStorageItem.GetKey()
+		{
+			return Url.ToLowerInvariant();
+		}
+
+	}
+}
